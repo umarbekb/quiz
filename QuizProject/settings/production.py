a01ab500af1e01a396dcb97100f35e5f0f657abf
bolshoysten2 <139250027+bolshoysten2@users.noreply.github.com>
2024-01-25 19:22:37 +0500

    first commit

--- QuizProject/settings/production.py
+++ QuizProject/settings/production.py
@@ -1,3 +1,5 @@
+from distutils.command.config import config
+
 from .base import *
 
 DEBUG = False
@@ -5,8 +7,12 @@ DEBUG = False
 ALLOWED_HOST = []
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '../db.sqlite3',
+    "default": {
+        "ENGINE": config("SQL_ENGINE"),
+        "NAME": config("SQL_DATABASE"),
+        "USER": config("SQL_USER"),
+        "PASSWORD": config("SQL_PASSWORD"),
+        "HOST": config("SQL_HOST"),
+        "PORT": config("SQL_PORT"),
     }
 }
